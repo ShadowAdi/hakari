1afcb68c7b38329b9d1eac11fa22ad4247323bf7
ShadowAdi <shadowshukla76@gmail.com>
2025-12-30 00:44:33 +0530

    Improve Docker File

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,14 @@ WORKDIR /app
 
 COPY package*.json ./
 
-COPY package-lock*.json ./
+RUN npm install
 
 COPY . .
+
+RUN npm run build
 
 ENV PORT = 3000
 
 EXPOSE 3000
 
-CMD [ "npm","start" ]
+CMD [ "npm", "start" ]
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log(`Hello`)
+import express, { Request, Response } from "express";
+
+const app = express()
+
+
+const PORT = process.env.PORT
+
+app.get(`/`, (req: Request, res: Response) => {
+    return res.status(200).json(`
+        Server Started`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT ${PORT}`)
+})
