22d172f98a0b1ab9aa558024b264c24706e9dcac
ShadowAdi <shadowshukla76@gmail.com>
2025-12-31 02:48:24 +0530

    quick fix

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from "express";
-import { PORT } from "./config/DotenvConfig.js";
 import { AppConnect } from "./config/AppConfig.js";
 const app = express()
 
 
 app.get(`/`, (req: Request, res: Response) => {
     return res.status(200).json({
-        "message": `Server Started at Port ${PORT}`
+        "message": `Server Started`
     })
 })
 
